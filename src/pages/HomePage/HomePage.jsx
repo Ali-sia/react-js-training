e378a3ec578b4c2815376c3cc88adbcce803b4c2
aliesia-zahorui <aliesia@ukr.net>
2023-03-15 12:21:02 +0200

    Create HomePage.jsx

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,57 @@
+const HomePage = () => {
+  return (
+    <>
+      <h2>home page</h2>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo vel sit
+        voluptatum accusantium fuga placeat nostrum, facilis soluta ratione ut
+        necessitatibus laudantium ipsum eveniet id, reiciendis tempora
+        perspiciatis consectetur neque!
+      </p>
+      <p>
+        Temporibus tempora laborum eius similique obcaecati! Harum quo maiores
+        necessitatibus est nostrum voluptatum dolorum doloribus tempora! Minus
+        facilis sed nihil non sequi reiciendis culpa laudantium? Non incidunt
+        earum amet commodi.
+      </p>
+      <p>
+        Temporibus inventore quasi molestiae modi porro odit similique ipsum,
+        nobis ipsam libero recusandae aliquid blanditiis doloribus tempore
+        voluptatum eos dolorum labore id officia esse ratione repudiandae!
+        Maxime ipsa dicta quaerat?
+      </p>
+      <p>
+        Ab, ea officiis doloremque consectetur ad unde perspiciatis eveniet
+        beatae architecto asperiores consequatur. Repellendus maxime ratione
+        consequatur, dolore debitis velit pariatur non at molestias ducimus
+        deleniti modi magni nam consectetur.
+      </p>
+      <p>
+        Nobis nulla natus harum impedit! Cupiditate facere consectetur
+        consequatur reiciendis, aliquam et fugiat numquam laudantium dignissimos
+        similique rem velit impedit omnis id commodi necessitatibus
+        reprehenderit quas molestiae doloribus excepturi quo.
+      </p>
+      <p>
+        Dolores voluptate quis suscipit eius accusantium facere tenetur fuga,
+        iusto architecto est consequuntur, possimus, quas vero veniam accusamus
+        nihil. Quae corrupti porro beatae a necessitatibus blanditiis deserunt,
+        dolorem ea esse.
+      </p>
+      <p>
+        Temporibus nesciunt ex quod illum doloribus soluta sapiente blanditiis
+        officiis. Officia ipsa, nulla laboriosam beatae nemo necessitatibus eos,
+        illum iure quo, minus distinctio amet blanditiis quos! Quibusdam
+        laudantium animi aspernatur.
+      </p>
+      <p>
+        Obcaecati culpa consectetur odio a, modi quod in rem neque perferendis
+        iste? Vero beatae odio omnis commodi, corporis hic aliquid assumenda
+        repellendus corrupti porro pariatur voluptas sed eum nulla quam?
+      </p>
+    </>
+  );
+};
+
+export default HomePage;
